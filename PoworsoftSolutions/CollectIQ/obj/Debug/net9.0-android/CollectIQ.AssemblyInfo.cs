67e4f575c937a2ea74320fdb5f3e9770cb89ad91
poworoznykd <122408216+poworoznykd@users.noreply.github.com>
2025-11-26 20:07:46 -0500

    Fixed problems

--- PoworsoftSolutions/CollectIQ/obj/Debug/net9.0-android/CollectIQ.AssemblyInfo.cs
+++ PoworsoftSolutions/CollectIQ/obj/Debug/net9.0-android/CollectIQ.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollectIQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b6391c2465310f373e3c0dce80e14ac2efd17944")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+736c0d47db08f9c9c02de8eccf69333577edbe76")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollectIQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollectIQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
